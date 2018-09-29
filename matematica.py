f8cf6d256694bcc0bc7780bf22caee277c82de45
andredeloque <andredeloque@gmail.com>
2018-09-29 01:48:01 -0300

    atividades

--- matematica.py
+++ matematica.py
@@ -2,13 +2,15 @@ a = float(input("insira o valor de a: "))
 b = float(input("insira o valor de b: "))
 c = float(input("insira o valor de c: "))
 delta = b**2-4*a*c
+raizDelta=delta**0.5
+x1 = (-b+raizDelta) / (2*a)
+x2 = (-b-raizDelta) / (2*a)
 if delta < 0:
-    print ("Delta menor que 0")
-else:
-    print ("delta = ", delta)
-    raizDelta=delta**0.5
-    print ("raiz de delta = ", raizDelta)
-    x1 = (-b+raizDelta) / (2*a)
-    print ("x1 = ", x1)
-    x2 = (-b-raizDelta) / (2*a)
-    print ("x2 = ", x2)
+    print ("esta equação não possui raízes reais")
+if delta == 0:
+    print ("a raiz desta equação é ", x1)
+if delta > 0:
+    if x1 < x2:
+        print ("as raízes da equação são X", x1,"e Y", x2)
+    else:
+        print ("as raízes da equação são Y", x2,"e X", x1)
