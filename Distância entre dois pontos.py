b392f539471964f9c37dc84a7c73456fad3082af
andredeloque <andredeloque@gmail.com>
2018-09-29 02:24:50 -0300

    atividades

--- Distância entre dois pontos.py
+++ Distância entre dois pontos.py
@@ -1,9 +1,9 @@
 import math
-x1 = float(input("Digite o primeiro numero para X: "))
-y1 = float(input("Digite o primeiro numero para Y: "))
-x2 = float(input("Digite o segundo numero para X: "))
-y2 = float(input("Digite o segundo numero para Y: "))
-distancia = math.sqrt ((x1-x2)**2) + ((y1-y2)**2)
+x1 = float(input("Digite o primeiro numero para X1: "))
+y1 = float(input("Digite o primeiro numero para Y1: "))
+x2 = float(input("Digite o segundo numero para X2: "))
+y2 = float(input("Digite o segundo numero para Y2: "))
+distancia = math.sqrt (((x2-x1)**2) + ((y2-y1)**2))
 print (distancia)
 if distancia >= 10:
     print ("longe")
